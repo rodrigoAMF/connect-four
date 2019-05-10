cce1626a64af3d5f1dd528c8f3c0059b1f8b21a1
rodrigoAMF <rodrigoamf@outlook.com>
2019-05-10 19:18:14 -0300

    Add funções responsáveis pelo click de cada coluna do jogo

--- js/game.js
+++ js/game.js
@@ -279,10 +279,6 @@ class Estado{
     }
 }
 
-function coluna0f(){
-    console.log("Coluna 1");
-}
-
 class QuatroEmLinha {
     estadoAtual = new Estado();
 
@@ -350,26 +346,58 @@ class QuatroEmLinha {
     }
 }
 
+function clickColuna0(){
+    alert("Você clicou na coluna 1!");
+}
+
+function clickColuna1(){
+    alert("Você clicou na coluna 2!");
+}
+
+function clickColuna2(){
+    alert("Você clicou na coluna 3!");
+}
+
+function clickColuna3(){
+    alert("Você clicou na coluna 4!");
+}
+
+function clickColuna4(){
+    alert("Você clicou na coluna 5!");
+}
+
+function clickColuna5(){
+    alert("Você clicou na coluna 6!");
+}
+
+function clickColuna6(){
+    alert("Você clicou na coluna 7!");
+}
+
+function clickColuna7(){
+    alert("Você clicou na coluna 0!");
+}
+
 let divName = "";
 for(let i=0; i < 8; i++) {
     for (let j=0; j < 8; j++) {
         divName = "posicao" + i + "-" + j;
         if (i === 0) {
-            document.getElementById(divName).addEventListener("click", coluna0f);
+            document.getElementById(divName).addEventListener("click", clickColuna0);
         } else if (i === 1) {
-            document.getElementById(divName).addEventListener("click", coluna0f);
+            document.getElementById(divName).addEventListener("click", clickColuna1);
         } else if (i === 2) {
-            document.getElementById(divName).addEventListener("click", coluna0f);
+            document.getElementById(divName).addEventListener("click", clickColuna2);
         } else if (i === 3) {
-            document.getElementById(divName).addEventListener("click", coluna0f);
+            document.getElementById(divName).addEventListener("click", clickColuna3);
         } else if (i === 4) {
-            document.getElementById(divName).addEventListener("click", coluna0f);
+            document.getElementById(divName).addEventListener("click", clickColuna4);
         } else if (i === 5) {
-            document.getElementById(divName).addEventListener("click", coluna0f);
+            document.getElementById(divName).addEventListener("click", clickColuna5);
         } else if (i === 6) {
-            document.getElementById(divName).addEventListener("click", coluna0f);
+            document.getElementById(divName).addEventListener("click", clickColuna6);
         } else if (i === 7) {
-            document.getElementById(divName).addEventListener("click", coluna0f);
+            document.getElementById(divName).addEventListener("click", clickColuna7);
         }
 
     }
