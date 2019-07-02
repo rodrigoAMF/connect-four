947f8afed81f3398750ced0a1c0855d02fef7d43
rodrigoAMF <rodrigoamf@outlook.com>
2019-07-02 19:02:59 -0300

    Update negamax, implementado poda alfa e beta
    Uhull

--- python/QuatroEmLinha.py
+++ python/QuatroEmLinha.py
@@ -6,19 +6,24 @@ import time
 class QuatroEmLinha:
     def __init__(self):
         self.estado_atual = Estado()
-        self.nivel_maximo_busca = 7
+        self.nivel_maximo_busca = 11
         self.nos_explorados = 0
 
     def encontrar_solucao(self):
         start = time.time()
         self.nos_explorados = 0
-        melhor_pontuacao, coluna_melhor_pontuacao = self.negamax(self.estado_atual)
+
+        alfa = -int((self.estado_atual.largura_tabuleiro*self.estado_atual.altura_tabuleiro)/2)
+        beta = int((self.estado_atual.largura_tabuleiro*self.estado_atual.altura_tabuleiro)/2)
+        melhor_pontuacao, coluna_melhor_pontuacao = self.negamax(self.estado_atual, alfa, beta)
+
         end = time.time()
         print(str(self.nos_explorados) + " Nós explorados em " + str(end - start) + " s")
+
         return melhor_pontuacao, coluna_melhor_pontuacao
 
     # Retorna pontuacao e melhor coluna para se jogar
-    def negamax(self, estado, nivel=1, nivel_max=True):
+    def negamax(self, estado, alfa, beta, nivel=1, nivel_max=True):
         self.nos_explorados += 1
         # Verifica se aconteceu um empate
         if estado.turno_atual == estado.altura_tabuleiro * estado.largura_tabuleiro:
@@ -36,17 +41,22 @@ class QuatroEmLinha:
                 return 50, -1
 
         # Inicia melhor pontuação com um valor muito pequeno
-        melhor_pontuacao = -estado.altura_tabuleiro * estado.largura_tabuleiro
+        valor_maximo = int((estado.altura_tabuleiro * estado.largura_tabuleiro)/2)
+        if beta > valor_maximo:
+            beta = valor_maximo
+            if alfa >= beta:
+                return beta, -1
         coluna_melhor_pontuacao = -1
-
         for coluna in range(estado.largura_tabuleiro):
             if estado.eh_possivel_jogar(coluna):
                 estado_novo = copy.deepcopy(estado)
                 estado_novo.jogar(coluna)
-                pontuacao, __ = self.negamax(estado_novo, nivel + 1, not nivel_max)
+                pontuacao, __ = self.negamax(estado_novo, -beta, -alfa, nivel + 1, not nivel_max)
                 pontuacao = -pontuacao
-                if pontuacao > melhor_pontuacao:
-                    melhor_pontuacao = pontuacao
+                if pontuacao >= beta:
+                    return pontuacao, coluna
+                if pontuacao > alfa:
+                    alfa = pontuacao
                     coluna_melhor_pontuacao = coluna
 
-        return melhor_pontuacao, coluna_melhor_pontuacao
+        return alfa, coluna_melhor_pontuacao
