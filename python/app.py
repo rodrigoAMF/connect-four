b4684de07388750dfd9882319989e6c96ac67e69
rodrigoAMF <rodrigoamf@outlook.com>
2019-07-03 18:48:38 -0300

    Update app.py para receber requisições do javascript
    Recebendo requisições via get com parametro jogadas recebendo uma string com as jogadas efetuadas

--- python/app.py
+++ python/app.py
@@ -8,41 +8,42 @@ from QuatroEmLinha import QuatroEmLinha
 app = Flask(__name__)
 CORS(app)
 
-def encontrar_melhor_jogada(tabuleiro):
-    posicao_jogada = np.random.randint(tabuleiro.shape[1])
-
-    while tabuleiro[0][posicao_jogada] != 0:
-        posicao_jogada = np.random.randint(tabuleiro.shape[1])
-
-    return posicao_jogada
 
 
-@app.route('/', methods=['GET', 'POST'])
-
+@app.route('/', methods=['GET'])
 def index():
+    jogo = QuatroEmLinha()
     jsonObj = {}
-    jsonObj['status'] = 200
 
-    tamanho_tabuleiro = request.args.get('tamanho_tabuleiro')
-    tabuleiro = request.args.get('tabuleiro')
-    if tamanho_tabuleiro is None or tabuleiro is None:
-        jsonObj['status'] = 500
-        jsonObj['errorMsg'] = "Os parametros tamanho_tabuleiro e tabuleiro sao obrigatorios!"
-        return json.dumps(jsonObj)
+    jogadas = request.args.get('jogadas')
 
-    tamanho_tabuleiro = np.array(tamanho_tabuleiro.split(",")).astype(np.int)
-    tabuleiro = np.array(tabuleiro.split(",")).astype(np.int)
+    jogo.estado_atual.carrega_sequencia_jogadas(jogadas)
 
-    if tabuleiro.shape[0] != tamanho_tabuleiro[0]*tamanho_tabuleiro[1]:
-        jsonObj['status'] = 500
-        jsonObj['errorMsg'] = "Os valores de tamanho_tabuleiro nao batem com o do tabuleiro passado"
-        return json.dumps(jsonObj)
+    pontuacao, coluna = jogo.encontrar_solucao()
 
-    tabuleiro = tabuleiro.reshape(tamanho_tabuleiro)
-
-    jsonObj['melhorColunaParaJogar'] = encontrar_melhor_jogada(tabuleiro)
+    jsonObj['pontuacao'] = pontuacao
+    jsonObj['coluna'] = coluna
 
     return json.dumps(jsonObj)
+
+'''
+@app.route('/vencedor', methods=['GET'])
+def index():
+    jogo = QuatroEmLinha()
+    jsonObj = {}
+
+    jogadas = request.args.get('jogadas')
+
+    jogo.estado_atual.carrega_sequencia_jogadas(jogadas)
+
+    # TODO: retornar vencedor certo
+    vencedor = jogo.estado_atual.eh_jogada_vitoriosa()
+
+    # TODO: dizer quem venceu
+    jsonObj['vencedor'] = vencedor
+
+    return json.dumps(jsonObj)
+'''
 
 if __name__ == "__main__":
     app.run()
