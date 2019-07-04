2c2a9788dde90e3d23fad5282b68a06e2155a6e3
rodrigoAMF <rodrigoamf@outlook.com>
2019-07-04 01:12:39 -0300

    Fix links da página escolha-dificuldade

--- js/nivelDificuldade.js
+++ js/nivelDificuldade.js
@@ -3,16 +3,16 @@ $(document).ready(function()
 	 $("#btnFacil").on("click",function(e){  
 	    e.preventDefault();
 	    sessionStorage.setItem("dificuldade", 1);
-	    window.location.href = "http://localhost/4-in-a-row/game.html";
+	    window.location.href = "game.html";
 	});
 	$("#btnMedio").on("click",function(e){  
 	    e.preventDefault();
 	    sessionStorage.setItem("dificuldade", 2);
-	    window.location.href = "http://localhost/4-in-a-row/game.html";
+	    window.location.href = "game.html";
 	});
 	$("#btnDificil").on("click",function(e){ 
 		e.preventDefault(); 
 		sessionStorage.setItem("dificuldade", 3);
-		window.location.href = "http://localhost/4-in-a-row/game.html";
+		window.location.href = "game.html";
 	});
  });
